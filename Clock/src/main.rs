d5497ce4fff1f3bc89a5d88c3a71761a8bc35c7f
pole9774 <81774748+pole9774@users.noreply.github.com>
2023-05-13 17:34:44 +0200

    Add files via upload

--- Clock/src/main.rs
+++ Clock/src/main.rs
@@ -1,11 +1,17 @@
 use clock::Clock;
 
 fn main() {
-    let mut c = Clock::new(0, 10);
+    let mut c = Clock::new(0, 50);
     println!("{}", c);
     c = c.add_minutes(-30);
     println!("{}", c);
 
     let clock = Clock::new(2, 20).add_minutes(-3000);
     println!("{}", clock);
+
+    if c == clock {
+        println!("Uguali");
+    } else {
+        println!("Diversi");
+    }
 }
