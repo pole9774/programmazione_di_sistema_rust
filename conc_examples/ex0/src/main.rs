02977a92a970b83d5cbb6262049fc05a811030f6
pole9774 <81774748+pole9774@users.noreply.github.com>
2023-05-26 14:06:20 +0200

    Add files via upload

---
+++ conc_examples/ex0/src/main.rs
@@ -0,0 +1,30 @@
+use std::thread;
+use std::thread::JoinHandle;
+use std::thread::sleep;
+use std::time::Duration;
+
+fn main() {
+    let v: Vec<JoinHandle<i32>> = (1..=4).map(|tid| {
+        thread::spawn(move || {
+            (1..10).for_each(|i| {
+                println!("Thread #{}, iterazione #{}", tid, i);
+                sleep(Duration::from_secs(1));
+            });
+            if tid == 3 {panic!("Computazione fallita");}
+            println!("Thread #{} fatto", tid);
+            tid
+        })
+    }).collect();
+
+    println!("Thread principale");
+    sleep(Duration::from_secs(4));
+    println!("Attendo terminazione thread secondari");
+    v.into_iter().for_each(|h| {
+        let res = h.join();
+        match res {
+            Ok(val) => println!("Res = {}", val),
+            Err(e) => println!("Error {:?}", e),
+        }
+    });
+    println!("Thread terminato");
+}
---
+++ conc_examples/ex2/src/main.rs
@@ -0,0 +1,65 @@
+use std::time::Duration;
+use std::thread;
+use std::thread::sleep;
+use std::sync::Arc;
+
+mod cdl {
+    use std::sync::{Condvar, Mutex};
+    
+    pub struct Cdl {
+        m: Mutex<usize>,
+        cv: Condvar
+    }
+
+    impl Cdl {
+        pub fn new(count: usize) -> Self {
+            Cdl {
+                m: Mutex::new(count),
+                cv: Condvar::new()
+            }
+        }
+
+        pub fn count_down(&self) {
+            let mut s = self.m.lock().unwrap();
+            if *s == 0 {
+                panic!("Count is already 0");
+            }
+            *s -= 1;
+            if *s == 0 {
+                self.cv.notify_all();
+            }
+        }
+
+        pub fn wait(&self) {
+            let mut s = self.m.lock().unwrap();
+            while *s > 0 {
+                s = self.cv.wait(s).unwrap();
+            }
+        }
+    }
+}
+
+fn main() {
+    let c = Arc::new(cdl::Cdl::new(2));
+    let cdl1 = Arc::clone(&c);
+    let cdl2 = Arc::clone(&c);
+
+    let t1 = thread::spawn(move || {
+        sleep(Duration::from_secs(2));
+        cdl1.count_down();
+        sleep(Duration::from_secs(2));
+        println!("Thread1 done");
+    });
+
+    let t2 = thread::spawn(move || {
+        sleep(Duration::from_secs(3));
+        cdl2.count_down();
+        sleep(Duration::from_secs(6));
+        println!("Thread1 done");
+    });
+
+    c.wait();
+    println!("Done");
+    t1.join().unwrap();
+    t2.join().unwrap();
+}
